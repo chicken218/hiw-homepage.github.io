f947fcf9df96af0935acb6c7f13abee205584afd
HIW <87884294+hiw-homepage@users.noreply.github.com>
2021-12-30 10:50:14 +0900

    Update hiw-homepage.js

--- hiw-homepage.js
+++ hiw-homepage.js
@@ -1,14 +1,22 @@
 document.getElementById("form");
 document.getElementById("join");
+document.getElementById("email");
 
 function Join() {
     form.style.display ="none";
     join.style.display ="block";
 }
 
+function Email() {
+    form.style.display ="none";
+    email.style.display ="block";
+    join.style.display ="none"
+}
+
 function Form() {
     form.style.display ="block";
     join.style.display ="none";
+    email.style.display ="none";
     window.alert("内容が送信されました。口頭またはメールにて返答を致します｡");
     var submitted = false;
 }
