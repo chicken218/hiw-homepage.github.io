c81973cb88bda5557940d00510e78d385f440415
HIW <87884294+hiw-homepage@users.noreply.github.com>
2021-12-31 10:24:14 +0900

    Update hiw-homepage.js

--- hiw-homepage.js
+++ hiw-homepage.js
@@ -2,6 +2,12 @@ document.getElementById("form");
 document.getElementById("join");
 document.getElementById("email");
 
+function Home() {
+    form.style.display ="block";
+    join.style.display ="none";
+    email.style.display ="none";
+}
+
 function Join() {
     form.style.display ="none";
     join.style.display ="block";
