281a07c1649f381a3eccc43874c230ca8ea9e6e9
HIW <87884294+hiw-homepage@users.noreply.github.com>
2022-01-01 08:44:11 +0900

    Update hiw-homepage.js

--- hiw-homepage.js
+++ hiw-homepage.js
@@ -1,27 +1,34 @@
 document.getElementById("form");
 document.getElementById("join");
 document.getElementById("email");
+document.getElementById("terms");
 
 function Home() {
-    form.style.display ="block";
-    join.style.display ="none";
-    email.style.display ="none";
+    form.style.display = "block";
+    join.style.display = "none";
+    email.style.display = "none";
+}
+
+function Terms() {
+    form.style.display = "none";
+    terms.style.display = "block";
 }
 
 function Join() {
-    form.style.display ="none";
-    join.style.display ="block";
+    form.style.display = "none";
+    terms.style.display = "none"
+    join.style.display = "block";
 }
 
 function Email() {
-    form.style.display ="none";
-    email.style.display ="block";
-    join.style.display ="none"
+    form.style.display = "none";
+    email.style.display = "block";
+    join.style.display = "none"
 }
 
 function Form() {
-    form.style.display ="block";
-    join.style.display ="none";
-    email.style.display ="none";
+    form.style.display = "block";
+    join.style.display = "none";
+    email.style.display = "none";
     var submitted = false;
 }
