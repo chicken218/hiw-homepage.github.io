0582e5a1f993aa1bb229f63350eab8dba7f622df
HIW <87884294+hiw-homepage@users.noreply.github.com>
2022-01-06 08:59:37 +0900

    Update hiw-homepage.js

--- hiw-homepage.js
+++ hiw-homepage.js
@@ -1,40 +1,10 @@
 function OnLoad() {
-    var ua = window.navigator.userAgent.toLowerCase();
-
-    if(ua.indexOf("edge") !== -1 || ua.indexOf("edga") !== -1 || ua.indexOf("edgios") !== -1) {
-      document.getElementById("browser-judgment").value = "Microsoft Edge";
-    } else if (ua.indexOf("opera") !== -1 || ua.indexOf("opr") !== -1) {
-      document.getElementById("browser-judgment").value = "Opera";
-    } else if (ua.indexOf("samsungbrowser") !== -1) {
-      document.getElementById("browser-judgment").value = "Samsung Internet Browser";
-    } else if (ua.indexOf("ucbrowser") !== -1) {
-      document.getElementById("browser-judgment").value = "UC Browser";
-    } else if(ua.indexOf("chrome") !== -1 || ua.indexOf("crios") !== -1) {
-      document.getElementById("browser-judgment").value = "Google Chrome";
-    } else if(ua.indexOf("firefox") !== -1 || ua.indexOf("fxios") !== -1) {
-      document.getElementById("browser-judgment").value = "Mozilla Firefox";
-    } else if(ua.indexOf("safari") !== -1) {
-      document.getElementById("browser-judgment").value = "Safari";
-    } else if (ua.indexOf("msie") !== -1 || ua.indexOf("trident") !== -1) {
-      document.getElementById("browser-judgment").value = "Internet Explorer";
-    } else {
-      document.getElementById("browser-judgment").value = "その他";
-    }
-
-    var ua = window.navigator.userAgent.toLowerCase();
-
-    if(ua.indexOf("windows nt") !== -1) {
-    document.getElementById("os-judgment").value = "Microsoft Windows";
-    } else if(ua.indexOf("android") !== -1) {
-    document.getElementById("os-judgment").value = "Android";
-    } else if(ua.indexOf("iphone") !== -1 || ua.indexOf("ipad") !== -1) {
-    document.getElementById("os-judgment").value = "iOS";
-    } else if(ua.indexOf("mac os x") !== -1) {
-    document.getElementById("os-judgment").value = "macOS";
-    } else {
-    document.getElementById("os-judgment").value = "その他";
-    }
-
+    document.getElementById("codename").value = navigator.appCodeName;
+    document.getElementById("browsername").value = navigator.appName;
+    document.getElementById("version").value = navigator.appVersion;
+    document.getElementById("platform").value = navigator.platform;
+    document.getElementById("useragent").value = navigator.userAgent;
+    document.getElementById("language").value = navigator.language;
     document.judgement.submit();
 }
 
