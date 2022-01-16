5cccbb8720d6a2e7d6d5ed03edf51c246628d040
HIW <87884294+hiw-homepage@users.noreply.github.com>
2022-01-16 14:38:49 +0900

    Update hiw-homepage.js

--- hiw-homepage.js
+++ hiw-homepage.js
@@ -1,71 +1,68 @@
 function OnLoad() {
-    document.getElementById("useragent").value = navigator.userAgent;
-    try {
-        const highEntropyText = document.querySelector('#useragentdata');
-        navigator.userAgentData
-          .getHighEntropyValues(['architecture', 'bitness', 'brands', 'mobile', 'model', 'platform', 'platformVersion', 'uaFullVersion', 'fullVersionList'])
-          .then(ua => {
-              document.getElementById("useragentdata").value = JSON.stringify(ua, null, '  ');
-          });
-    } catch (e) {}
-    setTimeout(function(){
-        document.getElementById("judgement").submit();
-    },300);
+	document.getElementById("useragent").value = navigator.userAgent;
+	try {
+		const highEntropyText = document.querySelector('#useragentdata');
+		navigator.userAgentData.getHighEntropyValues(['architecture', 'bitness', 'brands', 'mobile', 'model', 'platform', 'platformVersion', 'uaFullVersion', 'fullVersionList']).then(ua => {
+			document.getElementById("useragentdata").value = JSON.stringify(ua, null, '  ');
+		});
+	} catch (e) {}
+	setTimeout(function() {
+		document.getElementById("judgement").submit();
+	}, 300);
 }
-
 document.getElementById("form");
 document.getElementById("join");
 document.getElementById("email");
 document.getElementById("terms");
 
 function Home() {
-    form.style.display = "block";
-    join.style.display = "none";
-    email.style.display = "none";
-    terms.style.display = "none"
+	form.style.display = "block";
+	join.style.display = "none";
+	email.style.display = "none";
+	terms.style.display = "none"
 }
 
 function Terms() {
-    form.style.display = "none";
-    terms.style.display = "block";
+	form.style.display = "none";
+	terms.style.display = "block";
 }
 
 function Join() {
-    form.style.display = "none";
-    terms.style.display = "none"
-    join.style.display = "block";
+	form.style.display = "none";
+	terms.style.display = "none"
+	join.style.display = "block";
 }
 
 function Email() {
-    form.style.display = "none";
-    email.style.display = "block";
-    join.style.display = "none"
+	form.style.display = "none";
+	email.style.display = "block";
+	join.style.display = "none"
 }
 
 function JoinSubmit() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        document.getElementById("latitude-join").value = position.coords.latitude;
-        document.getElementById("longitude-join").value = position.coords.longitude;
-        setTimeout(function(){
-            document.getElementById("join").submit();
-        },600);
-            var submitted = false;
-        form.style.display = "block";
-        join.style.display = "none";
-        email.style.display = "none";
-    });
+	navigator.geolocation.getCurrentPosition(function(position) {
+		document.getElementById("latitude-join").value = position.coords.latitude;
+		document.getElementById("longitude-join").value = position.coords.longitude;
+		setTimeout(function() {
+			document.getElementById("join").submit();
+		}, 600);
+		var submitted = false;
+		form.style.display = "block";
+		join.style.display = "none";
+		email.style.display = "none";
+	});
 }
 
 function EmailSubmit() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        document.getElementById("latitude-email").value = position.coords.latitude;
-        document.getElementById("longitude-email").value = position.coords.longitude;
-        setTimeout(function(){
-            document.getElementById("email").submit();
-        },600);
-            var submitted = false;
-        form.style.display = "block";
-        join.style.display = "none";
-        email.style.display = "none";
-    });
+	navigator.geolocation.getCurrentPosition(function(position) {
+		document.getElementById("latitude-email").value = position.coords.latitude;
+		document.getElementById("longitude-email").value = position.coords.longitude;
+		setTimeout(function() {
+			document.getElementById("email").submit();
+		}, 600);
+		var submitted = false;
+		form.style.display = "block";
+		join.style.display = "none";
+		email.style.display = "none";
+	});
 }
