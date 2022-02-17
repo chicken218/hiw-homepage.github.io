4ee5637f255aeaf37ca54f5aaefbf3e61a7abc52
HIW ホームページ <87884294+hiw-homepage@users.noreply.github.com>
2022-02-18 07:41:30 +0900

    Add files via upload

---
+++ nyp/nyp.js
@@ -0,0 +1,20 @@
+document.getElementById("body");
+document.getElementById("header");
+document.getElementById("main");
+document.getElementById("link");
+document.getElementById("logo");
+document.getElementById("linkDiv");
+
+function OnLoad() {
+	const linkHeight = link.offsetHeight;
+    logo.setAttribute("height", linkHeight + "px");
+    logo.setAttribute("width", linkHeight * 1.5 + "px");
+    const headerHeight = header.offsetHeight + 64 + "px 0 64px";
+	main.style.margin = headerHeight;
+}
+
+$(function() {
+    $('#home-link').click(function() {
+        location.replace("https://hiw-homepage.github.io/hiw-homepage.html");
+    })
+})
