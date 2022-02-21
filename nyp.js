b9a154e49d9e3a14d67207ba5aacfa010f2192b1
HIW ホームページ <87884294+hiw-homepage@users.noreply.github.com>
2022-02-22 07:37:12 +0900

    Update nyp.js

--- nyp.js
+++ nyp.js
@@ -2,13 +2,13 @@ document.getElementById("body");
 document.getElementById("header");
 document.getElementById("main");
 document.getElementById("link");
-document.getElementById("logo");
+document.getElementById("svg71");
 document.getElementById("linkDiv");
 
 function OnLoad() {
 	const linkHeight = link.offsetHeight;
-    logo.setAttribute("height", linkHeight + "px");
-    logo.setAttribute("width", linkHeight * 1.5 + "px");
+    svg71.setAttribute("height", linkHeight + "px");
+    svg71.setAttribute("width", linkHeight * 1.5 + "px");
     const headerHeight = header.offsetHeight + 64 + "px 0 64px";
 	main.style.margin = headerHeight;
 }
